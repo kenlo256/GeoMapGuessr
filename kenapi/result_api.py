0a8137585c6eb1c204569ebdfe08cfdcf4503228
kenlo256 <68675014+kenlo256@users.noreply.github.com>
2021-11-07 20:48:41 +0000

    commit suicide, put request have to fix later

---
+++ kenapi/result_api.py
@@ -0,0 +1,54 @@
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+app = Flask(__name__)
+api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///result.db'
+
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+
+class Country(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    counter = db.Column(db.Integer)
+
+
+class CountrySchema(ma.SQLAlchemySchema):
+    class Meta:
+        model = Country
+
+    id = ma.auto_field()
+    name = ma.auto_field()
+    counter = ma.auto_field()
+
+
+db.create_all()
+
+parser = reqparse.RequestParser(bundle_errors=True)
+parser.add_argument('country', type=str, required=True, help='country name required')
+
+
+class Result(Resource):
+    def put(self, country):
+        args = parser.parse_args()
+        q = Country.query(Country).filter(Country.name == country)
+        if q.exists():
+            result = Country.query.filter_by(name=country)
+            result.counter += 1
+        else:
+            result = Country(name=args['country'], counter=1)
+        db.session.add(result)
+        db.session.commit()
+        country = CountrySchema()
+        return country.dump(result), 201
+
+
+api.add_resource(Result, "/result")
+
+
+if __name__ == '__main__':
+    app.run()

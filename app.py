c4a6b7c3c33f5616f5890c291cc9e6987c82a8af
kenlo256 <68675014+kenlo256@users.noreply.github.com>
2021-11-12 21:55:21 +0000

    trial for the random function to the mapquest api

--- app.py
+++ app.py
@@ -5,12 +5,34 @@ from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 Bootstrap(app)
-#lol
-BASEKEN = "http://119.246.146.149:5000/"
 
-responseken = requests.get(BASEKEN + "/hello")
+BASE = "http://geomapguessr.me:"
 
-print(responseken.json())
+geocodeQuality = "COUNTRY"
+mapQuestGetRequest = requests.get("http://www.mapquestapi.com/geocoding/v1/reverse?"
+                                  "key=P6SlkwXEUSNGa2y0MdU45AXA3LADkReB&location="
+                                  "-55.671610,-3.914930"
+                                  "&includeRoadMetadata=true&includeNearestIntersection=true");
+
+while geocodeQuality != "STREET":
+    responseOmer = requests.get(BASE + "5000/hello")
+    randCoordinate = responseOmer.json()
+
+    latitude = randCoordinate['latitude']
+    longitude = randCoordinate['longitude']
+
+    mapQuestGetRequest = requests.get("http://www.mapquestapi.com/geocoding/v1/reverse?"
+                                  "key=P6SlkwXEUSNGa2y0MdU45AXA3LADkReB&location="
+                                  + latitude + "," + longitude +
+                                  "&includeRoadMetadata=true&includeNearestIntersection=true");
+
+    randResult = mapQuestGetRequest.json()
+    geocodeQuality = randResult['results'][0]['locations'][0]['geocodeQuality']
+    print(geocodeQuality)
+
+print(mapQuestGetRequest.json())
+resultGetRequest = requests.get(BASE + "5001/Result")
+resultPutRequest = requests.put(BASE + "5001/Result")
 
 if __name__ == '__main__':
     app.run()

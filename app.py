07f986a0cd2b413d46889b576db45d911df5577e
kenlo256 <68675014+kenlo256@users.noreply.github.com>
2021-11-14 16:10:08 +0000

    incomplete in case u are deving

--- app.py
+++ app.py
@@ -1,39 +1,49 @@
-from flask import Flask
+from flask import Flask, render_template
 import requests
 from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
 Bootstrap(app)
+
 # filter out list of bad location for guessing
 badGeocode = ["COUNTY", "COUNTRY", "CITY", "STATE"]
 BASE = "http://geomapguessr.me:"
 
+@app.route("/")
+def home():
+    return render_template("index.html", content="Testing")
+
+
 # GUI button for random function
 
 # python doesn't have do while loops reeeeeee
-geocodeQuality = "COUNTRY"
-mapQuestGetRequest = requests.get("http://www.mapquestapi.com/geocoding/v1/reverse?"
+@app.route("/rand")
+def rand():
+    geocodequality = "COUNTRY"
+    mapquestgetrequest = requests.get("http://www.mapquestapi.com/geocoding/v1/reverse?"
                                   "key=P6SlkwXEUSNGa2y0MdU45AXA3LADkReB&location="
                                   "-55.671610,-3.914930"
                                   "&includeRoadMetadata=true&includeNearestIntersection=true");
 
-while geocodeQuality in badGeocode:
-    responseOmer = requests.get(BASE + "5000/hello")
-    randCoordinate = responseOmer.json()
+    while geocodequality in badGeocode:
+        responseomer = requests.get(BASE + "5000/hello")
+        randcoordinate = responseomer.json()
 
-    latitude = randCoordinate['latitude']
-    longitude = randCoordinate['longitude']
+        latitude = randcoordinate['latitude']
+        longitude = randcoordinate['longitude']
 
-    mapQuestGetRequest = requests.get("http://www.mapquestapi.com/geocoding/v1/reverse?"
+        mapquestgetrequest = requests.get("http://www.mapquestapi.com/geocoding/v1/reverse?"
                                   "key=P6SlkwXEUSNGa2y0MdU45AXA3LADkReB&location="
                                   + latitude + "," + longitude +
                                   "&includeRoadMetadata=true&includeNearestIntersection=true");
 
-    randResult = mapQuestGetRequest.json()
-    geocodeQuality = randResult['results'][0]['locations'][0]['geocodeQuality']
+        randresult = mapquestgetrequest.json()
+        geocodequality = randresult['results'][0]['locations'][0]['geocodeQuality']
 
-print(mapQuestGetRequest.json())
+    print(randresult)
+    return randresult['results'][0]['locations'][0]['mapUrl']
+
 
 # display the static map and change size=500,500 by changing the url
 

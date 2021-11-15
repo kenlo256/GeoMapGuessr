cd0be21a34e601ad9e62c6b1d963afd1d1447086
Omer Gulec <el19oeg@leeds.ac.uk>
2021-11-15 10:37:11 +0000

    doesnt work

--- omerapi/omer_api.py
+++ omerapi/omer_api.py
@@ -1,18 +1,27 @@
 import random
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse, abort
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///countries.db'
 
-
-# We kinda made a fatal mistake. We can not have two APIs running on the same URI...
+db = SQLAlchemy(app)
+class Countries(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(57), nullable=False)
 
 class Coordinates(Resource):
     def get(self):
         latitude = format(random.uniform(-90, 90), '.6f')
         longitude = format(random.uniform(-180, 180), '.6f')
         return {"latitude": latitude, "longitude": longitude}  # AMAZING API!!!! BEST API EVER!!
+
+    def put(self):
+        db.session.add(Countries(name="egrrth",id=3))
+        db.session.commit()
+        return ''
 
 
 api.add_resource(Coordinates, "/hello")
